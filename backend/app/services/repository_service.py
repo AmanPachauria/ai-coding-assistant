8c2013f84c709aa5ec9634fa68ac9e39e933e159
AmanPachauria <amanpachauria@gmail.com>
2026-07-17 18:41:46 +0530

    Implement GitHub repository validation and API integration

--- backend/app/services/repository_service.py
+++ backend/app/services/repository_service.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException
 import re
+import httpx
 
 def create_repository(url: str):
 
@@ -9,11 +10,22 @@ def create_repository(url: str):
     github_pattern = r"^https://github\.com/[^/]+/[^/]+/?$"
 
     if not re.match(github_pattern, url):
-        raise HTTPException(
-            status_code=400,
-            detail="Invalid GitHub repository URL."
-        )
-            
+        raise HTTPException( status_code=400, detail="Invalid GitHub repository URL." )
+    
+    # remove trailing slash if present
+    url = url.rstrip("/")
+    
+    # Split URL
+    parts = url.split("/")
+    
+    owner = parts[-2]
+    repository = parts[-1]
+
+    github_api_url = f"https://api.github.com/repos/{owner}/{repository}"
+
+    response = httpx.get(github_api_url, follow_redirects=True);
+    
+    return response.json()
 
     return {
         "message": "Repository URL received successfully",
