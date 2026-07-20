d2182a24791264ffb32b7fab70aaa2d1180b1920
AmanPachauria <amanpachauria@gmail.com>
2026-07-20 16:57:42 +0530

    Refactor repository service into reusable functions

--- backend/app/services/repository_service.py
+++ backend/app/services/repository_service.py
@@ -2,16 +2,18 @@ from fastapi import HTTPException
 import re
 import httpx
 
-def create_repository(url: str):
 
-    if url == "":
-        raise HTTPException(status_code = 400, detail = "Repository URL cannot be empty");
+def validate_repository_url(url: str):
+    if not url:
+        raise HTTPException(status_code=400, detail="Repository URL cannot be empty.")
 
     github_pattern = r"^https://github\.com/[^/]+/[^/]+/?$"
 
     if not re.match(github_pattern, url):
-        raise HTTPException( status_code=400, detail="Invalid GitHub repository URL." )
-    
+        raise HTTPException(status_code=400, detail="Invalid GitHub repository URL.")
+
+
+def get_owner_and_repo_from_url(url: str):
     # remove trailing slash if present
     url = url.rstrip("/")
     
@@ -21,13 +23,40 @@ def create_repository(url: str):
     owner = parts[-2]
     repository = parts[-1]
 
+    return owner, repository
+
+def fetch_repository_metadata(owner: str, repository: str):
     github_api_url = f"https://api.github.com/repos/{owner}/{repository}"
-
-    response = httpx.get(github_api_url, follow_redirects=True);
+    response = httpx.get(github_api_url, follow_redirects=True)
     
-    return response.json()
+    if response.status_code == 404:
+        raise HTTPException(status_code=404, detail="GitHub repository not found.")
+    
+    repository_data  = response.json()
 
+    return repository_data
+
+
+def map_repository_response(repository_data: dict):
     return {
-        "message": "Repository URL received successfully",
-        "url": url
+        "github_id": repository_data["id"],
+        "owner": repository_data["owner"]["login"],
+        "name": repository_data["name"],
+        "description": repository_data["description"],
+        "language": repository_data["language"],
+        "stars": repository_data["stargazers_count"],
+        "forks": repository_data["forks_count"],
+        "default_branch": repository_data["default_branch"],
+        "clone_url": repository_data["clone_url"]
     }
+
+
+def create_repository(url: str):
+
+    validate_repository_url(url) 
+
+    owner, repository = get_owner_and_repo_from_url(url)
+    
+    repository_data = fetch_repository_metadata(owner, repository)
+
+    return map_repository_response(repository_data)
